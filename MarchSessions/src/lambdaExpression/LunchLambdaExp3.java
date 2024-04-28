7d1d25d814c827eb04ac759da0105be9d7697698
Hassan <kkromah12@gmail.com>
2024-04-29 00:54:05 +0300

    Lambda Expression Started

---
+++ MarchSessions/src/lambdaExpression/LunchLambdaExp3.java
@@ -0,0 +1,17 @@
+package lambdaExpression;
+
+interface CLS {
+    int getLength(String str);
+}
+class LOS implements CLS {
+
+    @Override
+    public int getLength(String str) {
+        return 0;
+    }
+}
+public class LunchLambdaExp3 {
+    public static void main(String[] args) {
+
+    }
+}
