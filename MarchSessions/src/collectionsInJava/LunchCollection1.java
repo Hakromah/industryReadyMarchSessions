cc281d5b4fd6834201a18ce77168e1ead1af67cc
Hassan <kkromah12@gmail.com>
2024-06-03 19:42:07 +0300

    MultiThreading continue

---
+++ MarchSessions/src/collectionsInJava/LunchCollection1.java
@@ -0,0 +1,15 @@
+package collectionsInJava;
+
+import java.util.ArrayList;
+
+public class LunchCollection1 {
+
+	public static void main(String[] args) {
+		// Classes available in Collections
+		//--> ArrayList; LinkedList; ArrayDeque; ThreeSet; HasSet; LinkedHasSet...
+		
+		ArrayList<String> list = new ArrayList<>();
+
+	}
+
+}
