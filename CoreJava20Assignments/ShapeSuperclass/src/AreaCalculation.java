4a8d6edacf468c41132991d816be0398cab69bb7
Hassan <kkromah12@gmail.com>
2024-06-06 08:54:21 +0300

    Area calculation from abstract superclass

--- CoreJava20Assignments/ShapeSuperclass/src/AreaCalculation.java
+++ CoreJava20Assignments/ShapeSuperclass/src/AreaCalculation.java
@@ -1,49 +1,79 @@
 import java.awt.*;
 
-class Shape {
-	private double length;
-	private double width;
-	private double radius;
-	private double PI = 3.14;
+abstract class Shape {
 
-	public Shape(double length, double width, double radius, double PI) {
-		this.length = length;
-		this.width = width;
-		this.radius = radius;
-		this.PI = PI;
-	}
+    // Calculation of area
+    public abstract double calculateArea();
 
-	public void rectangleArea() {
-		System.out.println("Rectangle Area: " + length * width);
-	}
-
-	public void circleArea() {
-		System.out.println("Circle Area: " + PI * radius * radius);
-	}
-
-	public void perimeterOfRectangle() {
-		System.out.println("Perimeter of Rectangle: " + 2 * (length + width));
-	}
-
-	public void perimeterOfCircle() {
-		System.out.println("Perimeter of Circle: " + 2 * PI * radius);
-	}
-
+    //Calculation of Perimeter
+    public abstract double calculatePerimeter();
 }
 
-
-
+//Circle Class
 class Circle extends Shape {
+    private double radius;
+    private double PI = 3.14;
 
-	public Circle(double length, double width, double radius, double PI) {
-		super(length, width, radius, PI);
-	}
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * (radius * radius);
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * PI * radius;
+    }
 }
 
+//Rectangle class
+class Rectangle extends Shape {
 
+    private double length;
+    private double width;
 
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+//
 public class AreaCalculation {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	}
+        Shape trg = new Rectangle(50, 30);
+        Shape circle = new Circle(85);
+
+
+        double triangle = trg.calculateArea();
+        System.out.println("Triangle Measurements");
+        System.out.println("Area: " + triangle);
+
+        double perimeter = trg.calculatePerimeter();
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("♦".repeat(30) + " ");
+
+        System.out.println("Circle Area & Perimeter:");
+        double area = circle.calculateArea();
+        System.out.println("Area: " + area);
+
+        double perim = circle.calculatePerimeter();
+        System.out.println("Perimeter: " + perim);
+
+
+    }
 }
