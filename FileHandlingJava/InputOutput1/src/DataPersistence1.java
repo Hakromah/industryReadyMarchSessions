b15ece123b0de2eead76ac168b026af70b72f509
Hassan <kkromah12@gmail.com>
2024-06-11 09:19:19 +0300

    Edited

--- FileHandlingJava/InputOutput1/src/DataPersistence1.java
+++ FileHandlingJava/InputOutput1/src/DataPersistence1.java
@@ -1,43 +1,44 @@
 import java.io.File;
 
 public class DataPersistence1 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		/*
 		==> JavaApp => JVM => JRE => RAM
 		Data Persistence
 		1. File System => HDD => IO | File handling
-		2. Database =>JDBC
+		2. Database => JDBC
 		3. Network
 		4. Cloud
 		5. Memory
-		* */
-		String path1 = "C:\\Users\\pc\\IO\\FileHandling\\java.txt";
-		String path2 = "C:\\Users\\pc\\IO\\telusko";
-		File file1 = new File(path1);
-		try {
-			//System.out.println(file1.createNewFile());// if file exists, it will not create duplicate file
-			System.out.println(file1.exists());//false// true
-			System.out.println(file1.isFile());
-			System.out.println(file1.isDirectory());
-			System.out.println(file1.getPath());
+		*/
+        //String path1 = "C:\\Users\\pc\\IO\\FileHandling\\java.txt";
+        //String path2 = "C:\\Users\\pc\\IO\\telusko";
+        String path1 = "D:\\IO\\src\\java.txt";
+        String path2 = "D:\\IO\\telusko";
+        try {
+            File file1 = new File(path1);
+            //System.out.println(file1.createNewFile());// if file exists, it will not create duplicate file
+            System.out.println(file1.exists());//false// true
+            System.out.println(file1.isFile());
+            System.out.println(file1.isDirectory());
+            System.out.println(file1.getPath());
 
-			File directory = new File(path2);
-			System.out.println(directory.mkdir());
-			System.out.println("*".repeat(30));
+            File directory = new File(path2);
+            System.out.println(directory.isDirectory());
+            System.out.println("*".repeat(30));
 
-			File file2 = new File("C:\\Users\\pc\\IO\\src\\teluskojava.txt");
-			System.out.println(file2.createNewFile());
-			String[] str = file1.list();
-			int count = 0;
-			for (String files : str) {
-				count++;
-				System.out.println(files);
-			}
-			System.out.println("Number of files in the specified path :" + count);
-
-
-		} catch (Exception e) {
-			System.out.println("Something went wrong");
-		}
-	}
+            //File file2 = new File("C:\\Users\\pc\\IO\\src\\teluskojava.txt");
+            File file2 = new File("D:\\IO\\src");
+            System.out.println(file2.createNewFile());
+            String[] str = file1.list();
+            int count = 0;
+            for (String files : str) {
+                count++;
+                System.out.println(files);
+            }
+            System.out.println("Number of files in the specified path :" + count);
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+    }
 }
